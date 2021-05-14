fdf93b2e15787aecd5d459863a02f020103d7637
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-05-14 15:25:26 +0100

    ✨ PIC-1328 add session start time to Case (#13)
    * ✨ PIC-1328 add session start time to Case
    
    * ✨ PIC-1328 lint fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/model/externaldocumentrequest/Case.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/model/externaldocumentrequest/Case.kt
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
@@ -86,4 +87,9 @@ data class Case(
   val courtCode: String
     @JsonGetter
     get() = (block.session.courtCode)
+
+  val sessionStartTime: LocalDateTime
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+    @JsonGetter
+    get() = (block.session.getSessionStartTime())
 }
