eeb02b91a67fb8368a661a2886cae03beaef3755
John Evans <john.evans@digital.justice.gov.uk>
2021-06-22 19:42:10 +0100

    ✨ PIC-1493: Add messageType to court case message attributes (#19)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/service/MessageNotifierTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/service/MessageNotifierTest.kt
@@ -1,16 +1,20 @@
 package uk.gov.justice.digital.hmpps.courtlistsplitter.service
 
 import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.model.PublishRequest
 import com.amazonaws.services.sns.model.PublishResult
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -19,7 +23,7 @@ import org.springframework.context.annotation.Import
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import uk.gov.justice.digital.hmpps.courtlistsplitter.model.externaldocumentrequest.buildCase
 
-@ExtendWith(SpringExtension::class)
+@ExtendWith(SpringExtension::class, MockitoExtension::class)
 @Import(MessageNotifierTest.TestNotifierConfig::class)
 internal class MessageNotifierTest {
 
@@ -32,18 +36,24 @@ internal class MessageNotifierTest {
   @Autowired
   private lateinit var amazonSNSClient: AmazonSNS
 
+  @Captor
+  private lateinit var publishRequest: ArgumentCaptor<PublishRequest>
+
   @Test
   fun `when get case then serialise and publish to SNS`() {
 
     val case = buildCase(null, null, null)
     val result = PublishResult().withMessageId("messageId")
-    whenever(amazonSNSClient.publish(eq("topicArn"), ArgumentMatchers.contains("caseNo")))
+    whenever(amazonSNSClient.publish(any()))
       .thenReturn(result)
 
     messageNotifier.send(case, "message-id")
 
     verify(telemetryService).trackCourtCaseSplitEvent(case, "message-id")
-    verify(amazonSNSClient).publish(eq("topicArn"), ArgumentMatchers.contains("caseNo"))
+    verify(amazonSNSClient).publish(publishRequest.capture())
+    assertThat(publishRequest.value.message).contains("caseNo")
+    assertThat(publishRequest.value.messageAttributes["messageType"]?.dataType).isEqualTo("String")
+    assertThat(publishRequest.value.messageAttributes["messageType"]?.stringValue).isEqualTo("LIBRA_COURT_CASE")
   }
 
   @TestConfiguration
