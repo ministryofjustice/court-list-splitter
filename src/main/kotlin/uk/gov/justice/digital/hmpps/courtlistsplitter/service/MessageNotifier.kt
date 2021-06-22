103ec181d15dc370134a20660a4306704265e31f
John Evans <john.evans@digital.justice.gov.uk>
2021-06-22 14:23:12 +0100

    ✨ PIC-1493: Add messageType to court case message attributes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/service/MessageNotifier.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courtlistsplitter/service/MessageNotifier.kt
@@ -1,12 +1,16 @@
 package uk.gov.justice.digital.hmpps.courtlistsplitter.service
 
 import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.model.MessageAttributeValue
+import com.amazonaws.services.sns.model.PublishRequest
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.courtlistsplitter.model.externaldocumentrequest.Case
+
+private const val MESSAGE_TYPE = "LIBRA_COURT_CASE"
 
 @Component
 class MessageNotifier(
@@ -24,7 +28,14 @@ class MessageNotifier(
     val message = objectMapper.writeValueAsString(case)
     val subject = "Details for case " + case.caseNo + " in court " + case.courtCode + " published with messageId " + messageId
 
-    val publishResult = amazonSNSClient.publish(topicArn, message)
+    val messageValue = MessageAttributeValue()
+      .withDataType("String")
+      .withStringValue(MESSAGE_TYPE)
+
+    val publishRequest = PublishRequest(topicArn, message)
+      .withMessageAttributes(mapOf("messageType" to messageValue))
+
+    val publishResult = amazonSNSClient.publish(publishRequest)
     log.info("Published message with subject {} with message Id {}", subject, publishResult.messageId)
   }
 
