6a3a44cea2f09071ded5ad7854b26823fede4c52
John Evans <john.evans@digital.justice.gov.uk>
2022-02-18 12:27:30 +0000

    ⬆ ️PIC-1446: Update transitive dependencies with vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,18 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web:2.6.3") {
     implementation("org.apache.logging.log4j:log4j-api:2.17.1")
     because("vulnerability in packaged version 2.14.1")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.58")
+    implementation("org.apache.tomcat.embed:tomcat-embed-el:9.0.58")
+    implementation("org.apache.tomcat.embed:tomcat-embed-websocket:9.0.58")
+    because("vulnerability in packaged version 9.0.56")
   }
   implementation("org.springframework.boot:spring-boot-starter-validation:2.6.2") {
     implementation("org.apache.logging.log4j:log4j-api:2.17.1")
     because("vulnerability in packaged version 2.14.1")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:9.0.58")
+    implementation("org.apache.tomcat.embed:tomcat-embed-el:9.0.58")
+    implementation("org.apache.tomcat.embed:tomcat-embed-websocket:9.0.58")
+    because("vulnerability in packaged version 9.0.56")
   }
   implementation("com.microsoft.azure:applicationinsights-spring-boot-starter:2.6.4")
   implementation("com.amazonaws:aws-java-sdk-sqs:$awsSdkVersion")
