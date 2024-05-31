52e956c8cac5118b9798345adf468ccdeded5c04
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2024-05-31 16:19:30 +0100

    Update sqs queue for testing

--- src/test/resources/localstack/setup-sqs.sh
+++ src/test/resources/localstack/setup-sqs.sh
@@ -15,7 +15,7 @@ aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "h
 # The topic that the splitter writes to
 aws --endpoint-url http://localhost:4566 sqs create-queue --queue-name court-case-matcher-queue
 aws --endpoint-url=http://localhost:4566 sns create-topic --name court-case-events-topic
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-case-events-topic" --protocol "sqs" --notification-endpoint "http://localhost:4566/000000000000/court-case-matcher-queue"
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-case-events-topic" --protocol "sqs" --notification-endpoint "http://sqs.eu-west-2.localhost.localstack.cloud:4566/000000000000/court-case-matcher-queue"
 
 
 
