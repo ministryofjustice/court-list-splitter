6a683d104d272c8c4bd52db930fb3366c0798269
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-03-25 13:32:15 +0000

    PIC-3705 Fix broken aws setup script

--- src/test/resources/localstack/setup-sqs.sh
+++ src/test/resources/localstack/setup-sqs.sh
@@ -10,13 +10,11 @@ export PAGER=
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name crime-portal-gateway-queue-dlq
 aws --endpoint-url http://localhost:4566 sqs create-queue --queue-name crime-portal-gateway-queue
 
-aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/crime-portal-gateway-queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"2\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:crime-portal-gateway-queue-dlq\"}", "VisibilityTimeout": "0" }'
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/000000000000/crime-portal-gateway-queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"2\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:crime-portal-gateway-queue-dlq\"}", "VisibilityTimeout": "0" }'
 
 # The topic that the splitter writes to
 aws --endpoint-url http://localhost:4566 sqs create-queue --queue-name court-case-matcher-queue
 aws --endpoint-url=http://localhost:4566 sns create-topic --name court-case-events-topic
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-case-events-topic" --protocol "sqs" --notification-endpoint "http://localhost:4566/000000000000/court-case-matcher-queue"
-
-
+aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-case-events-topic" --protocol "sqs" --notification-endpoint "arn:aws:sns:eu-west-2:000000000000:court-case-matcher-queue"
 
 echo "SNS and SQS Configured"
